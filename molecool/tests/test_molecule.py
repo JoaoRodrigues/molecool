05c1174b1919d4024850a3569e8135d3771f4607
Joao Rodrigues <j.p.g.l.m.rodrigues@gmail.com>
2020-05-27 16:33:12 -0700

    added tests #1

---
+++ molecool/tests/test_molecule.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pytest
+
+import molecool
+
+
+def test_build_bond_failure():
+
+    coordinates = np.array([
+        [1, 1, 1],
+        [2.4, 1, 1],
+        [-0.4, 1, 1],
+        [1, 1, 2.4],
+        [1, 1, -0.4],
+    ])
+
+    with pytest.raises(ValueError):
+        bonds = molecool.build_bond_list(coordinates, min_bond=-1)
